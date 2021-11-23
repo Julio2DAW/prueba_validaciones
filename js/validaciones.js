d94fb7e1931619996321ad6dd981b2199c403656
Julio2DAW <jramosgago.guadalupe@alumnado.fundacionloyola.net>
2021-11-23 10:23:49 +0100

    Ejercicios: 1,2 y 4

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,3 +1,78 @@
+/**
+ * validaciones.js
+ * Examen de validaciones
+ */
 'use strict'
+window.onload=iniciar
 
+//Función iniciar
+function iniciar(){
+    //Ejercicio 2
+    //Select
+    let select = document.getElementById('sCurso')
+    select.onchange=elegirCursos
+
+    //Ejercicio 4
+     let ponerACero=document.querySelector("#btnCero")
+     ponerACero.onclick=cero
+}
+
+//Ejercicio 2
+//Función para elegir Curso
+function elegirCursos() {
+    if (document.getElementById('sCurso').value==1) {
+ 
+        //Creo el select
+        let select=document.createElement('select')
+        select.id='modulo'
+        select.name='modulo'
+
+        let div=document.getElementsByClassName('select')[0]
+        div.appendChild(select)
+
+        //Array de los Módulos
+        let modulos=["Programacion", "Lenguaje de Marca", "Base de Datos", "Entorno de Desarrollo", "Sistemas"]
+        //Ordenar Array alfabeticamente
+        modulos.sort()
+
+        //Recorrer el Array
+        for(let modulo in modulos){
+            let opcion=document.createElement('option')
+            opcion.text=modulos[modulo]
+            select.appendChild(opcion)
+        }
+    }else{
+        if (document.getElementById('sCurso').value==2) {
+ 
+            //Creo el select
+            let select=document.createElement('select')
+            select.id='modulo2'
+            select.name='modulo2'
+    
+            let div=document.getElementsByClassName('select')[0]
+            div.appendChild(select)
+    
+            //Array de los Módulos
+            let modulos2=["DIW", "DEAP", "DWES"]
+            //Ordenar Array alfabeticamente
+            modulos2.sort()
+    
+            //Recorrer el Array
+            for(let modulo2 in modulos2){
+                let opcion=document.createElement('option')
+                opcion.text=modulos2[modulo2]
+                select.appendChild(opcion)
+            }
+        }
+    }
+    
+}
+
+//Ejercicio 4
+//Inicializar type=number a 0
+function cero(){
+    for(let i=1; i<=3; i++){
+        document.querySelector("#iCalculo"+i).value=0;
+    }
+}
 console.log('Cargado JS')
